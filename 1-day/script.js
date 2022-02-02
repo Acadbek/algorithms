947c25b3ece2d0775bf020978ce1aad8a9532c7b
Asadbek <a.nosirjonov@uic.group>
2022-02-02 17:00:50 +0500

    delegatsia

--- 1-day/script.js
+++ 1-day/script.js
@@ -6,10 +6,20 @@ btn.forEach(btn => {
     btn.style.padding = '40px';
 })
 
-// agar wrapperni ichiga umuman yangi btn qo`shilsa ham u delegatsiya bo`ladi.
-// ya`ni 'cliked' so`zi faqat btn class bor buttonlarni uchun va yangi qoshilga hamma buttonlarga ishlaydi. 
+// agar wrapperni ichiga umuman yangi button qo`shilsa ham u delegatsiya bo`ladi.
+// ya`ni 'cliked' so`zi faqat .btn class bor buttonlarni uchun va yangi qo`shilga hamma buttonlarga ishlaydi. 
 
 wrapper.addEventListener('click', (e) => {
+    if (e.target && e.target.tagName == 'BUTTON') {
+        console.log('clicked');
+    }
+})
+
+// agar butun project bo`yicha faqat tanlab olingan buttonlarni ishlatmoqchi bo`lsam pasdagi yoziladi
+
+const body = document.querySelector('body');
+
+body.addEventListener('click', (e) => {
     if (e.target && e.target.tagName == 'BUTTON') {
         console.log('clicked');
     }
