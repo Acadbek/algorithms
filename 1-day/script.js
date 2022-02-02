43b02cda6a9f2f495c215b8a98b8b9edf2c3f509
Asadbek <a.nosirjonov@uic.group>
2022-02-02 16:46:36 +0500

    delegatsia

--- 1-day/script.js
+++ 1-day/script.js
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+const wrapper = document.querySelector('.wrapper');
+const btn = document.querySelectorAll('.btn');
+
+btn.forEach(btn => {
+    btn.style.backgroundColor = 'red'
+    btn.style.padding = '40px';
+})
+
+// agar wrapperni ichiga umuman yangi btn qo`shilsa ham u delegatsiya bo`ladi.
+// ya`ni 'cliked' so`zi faqat btn class bor buttonlarni uchun va yangi qoshilga hamma buttonlarga ishlaydi. 
+
+wrapper.addEventListener('click', (e) => {
+    if (e.target && e.target.tagName == 'BUTTON') {
+        console.log('clicked');
+    }
+})
