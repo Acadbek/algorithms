49652236eafefcff8f83b19f88122ce247c98ab0
Asadbek <a.nosirjonov@uic.group>
2022-03-05 18:10:32 +0500

    feat: palindrom func

--- 6-day/callStack.js
+++ 6-day/callStack.js
@@ -1,31 +1,4 @@
-// 'use strict';
-// const p = document.querySelector('#text')
-// const input = document.querySelector('#input')
-// const btn = document.querySelector('#btn')
-
-// function reverseFunc() {
-//     btn.addEventListener('click', () => {
-//         const revArr = []
-//         const valueLength = input.value.length - 1
-//         for (let i = valueLength; i >= 0; i--) {
-//             revArr.push(input.value[i])
-//         }
-//         let revValue = revArr.join('')
-//         let ourValue = input.value
-//         console.log(revValue)
-//         console.log(ourValue)
-
-//         if (revValue == ourValue) {
-//             console.log(true)
-//         }
-//         else {
-//             console.log(false)
-//         }
-//     })
-// }
-
-// reverseFunc()
-
+//  PAlindrome function
 
 function reverX(x) {
     const array = []
@@ -42,20 +15,3 @@ function reverX(x) {
 }
 
 reverX(121)
-
-
-// const isPalindrome = nums => {
-//     let length = Math.floor(Math.log(nums) / Math.log(10) + 1);
-//     while (length > 0) {
-//         let last = Math.abs(nums − Math.floor(nums / 10) * 10);
-//         let first = Math.floor(nums / Math.pow(10, length −1));
-//         if (first != last) {
-//             return false;
-//         };
-//         nums −= Math.pow(10, length−1) * first;
-//         nums = Math.floor(nums / 10);
-//         length −= 2;
-//     };
-//     return true;
-// };
-// isPalindrome
