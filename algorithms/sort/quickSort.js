0e349f98445e212686b34ea243be01e904b0a547
Asadbek <a.nosirjonov@uic.group>
2022-03-24 13:03:58 +0500

    feat: quicksort

---
+++ algorithms/sort/quickSort.js
@@ -0,0 +1,20 @@
+function quicksort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    let pivot = array[0];
+
+    let left = [];
+    let right = [];
+
+    for (let i = 1; i < array.length; i++) {
+        array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+    }
+
+    return quicksort(left).concat(pivot, quicksort(right));
+};
+
+let unsorted = [23, 45, 16, 37, 3, 99, 22, 100, 0, 3, 3, 5, 6, 2, 34, 45, 6, 45, 234, 456,];
+let sorted = quicksort(unsorted);
+console.log('Sorted array', sorted);
