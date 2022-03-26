909a1ab2b642944c70d64ea93ff45d5823ce5be0
Asadbek <a.nosirjonov@uic.group>
2022-03-26 16:27:41 +0500

    feat: pairs of two characters

--- codewars/pairsOfTwoCharacters.js
+++ codewars/pairsOfTwoCharacters.js
@@ -1,6 +1,6 @@
 // Complete the solution so that it splits the string into
 // pairs of two characters. If the string contains an odd 
-// number of characters then it should replace the missing 
+// number of characters then it should replace the missing.
 // second character of the final pair with an underscore ('_').
 
 
