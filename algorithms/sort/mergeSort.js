421b8633e4429a495ef2496d53640d8f9ec8ecf0
Asadbek <a.nosirjonov@uic.group>
2022-03-26 15:15:47 +0500

    feat: get camel case word

---
+++ algorithms/sort/mergeSort.js
@@ -0,0 +1,20 @@
+// Merge sort tez algo xisoblanadi.
+// (Divide and Conquer) usuli yordamida ishlaydigan tartiblash algosi.
+// ishlash tartibi royxatni 2ga bolib, 
+//  ikki tarafni alohida tartiblab jamlash asosida ishlaydi.
+
+// quicksort bn merge sortni silishtirsak BigO tarafdan birxil (O(nlog2n))
+//   yani bajaradigan operatsiyalar soni bir xil lekin bajarilish vaqti 
+//   xarxil. (quicksort kamroq vaqt sarflaydi).
+
+
+function mergeSort(array) {
+    let start = 0
+    let end = array.length - 1
+    let mid = Math.floor((start + end) / 2)
+    console.log(mid, 'merge-sort')
+}
+
+mergeSort([1, 5, 1, 3, 6, 8, 3, 1, 3, 5, 6, 7, 3, 23, 56, 778, 12, 34, 78, 13, 234, 56])
+
+// tugallanmagan
---
+++ codewars/camelCase.js
@@ -0,0 +1,7 @@
+function solution(word) {
+    let newWord = word.split(' ')
+    let upperLetter = newWord[1].charAt(0).toUpperCase()
+    console.log(`${newWord[0]}${upperLetter}${newWord[1].substring(1)}`)
+}
+
+solution('camel case')
