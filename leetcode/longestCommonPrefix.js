b079eb43acf7d16819cedc60d0d4c6f4e4b7af71
Asadbek <a.nosirjonov@uic.group>
2022-03-27 02:03:43 +0500

    feat: longest common prefix

--- leetcode/longestCommonPrefix.js
+++ leetcode/longestCommonPrefix.js
@@ -3,24 +3,45 @@
 // If there is no common prefix, return an empty string "".
 
 
+// function longestCommonPrefix(strs) {
+//     let arr = []
+//     for (let i = 0; i < strs.length; i++) {
+//         let letter = strs[i].split('')
+//         // console.log(strs[i].charAt(letter[0]) === strs[i].charAt(letter[0]))
+//         // console.log(letter[0])
+//     }
+
+
+//     strs.forEach(element => {
+//         for (let i = 0; i < element.length; i++) {
+//             // console.log(element[0] === element[i])
+//             // if (element[0] === element[i]) {
+//             arr.push(element[0] === element[i] ? element : '')
+//             // }
+//         }
+//     });
+//     console.log(arr)
+// };
+
+// longestCommonPrefix(['sas', 'cas', 'fas'])
+
+
 function longestCommonPrefix(strs) {
-    let arr = []
-    for (let i = 0; i < strs.length; i++) {
-        let letter = strs[i].split('')
-        // console.log(strs[i].charAt(letter[0]) === strs[i].charAt(letter[0]))
-        // console.log(letter[0])
-    }
+    // Return early on empty input
+    if (!strs.length) return '';
 
-
-    strs.forEach(element => {
-        for (let i = 0; i < element.length; i++) {
-            // console.log(element[0] === element[i])
-            if (element[0] === element[i]) {
-                arr.push(element[0] === element[i] ? element[i] : '')
-                console.log(arr)
+    // Loop through the letters of the first string
+    for (let i = 0; i <= strs[0].length; i++) {
+        // Loop through the other strings
+        for (let j = 1; j < strs.length; j++) {
+            // Check if this character is also present in the same position of each string
+            if (strs[0][i] !== strs[j][i]) {
+                // If not, return the string up to and including the previous character
+                return strs[0].slice(0, i);
             }
         }
-    });
+    }
+    console.log(strs[0]);
 };
 
-longestCommonPrefix(['asad', 'asal', 'asab'])
+longestCommonPrefix('asad', 'asc', 'asew')
