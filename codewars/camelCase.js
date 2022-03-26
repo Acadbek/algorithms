daa3413303a3eaf9e412a718fc6d531853e514a9
Asadbek <a.nosirjonov@uic.group>
2022-03-26 16:28:11 +0500

    feat: camelCase

--- codewars/camelCase.js
+++ codewars/camelCase.js
@@ -1,7 +1,7 @@
 function solution(word) {
     let newWord = word.split(' ')
     let upperLetter = newWord[1].charAt(0).toUpperCase()
-    console.log(`${newWord[0]}${upperLetter}${newWord[1].substring(1)}`, '=====CamelCase function======')
+    console.log(`${newWord[0]}${upperLetter}${newWord[1].substring(1)}`, '======CamelCase function======')
 }
 
 solution('camel case')
