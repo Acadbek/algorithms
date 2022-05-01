9078d606bec392540e433f789f849bb6f23b5d5f
Asadbek <a.nosirjonov@uic.group>
2022-05-01 11:41:31 +0500

    length of last word

--- leetcode/lengthOfLastWord.js
+++ leetcode/lengthOfLastWord.js
@@ -1,3 +1,9 @@
+// Given a string s consisting of some 
+// words separated by some number of spaces, 
+// return the length of the last word in the string.
+// A word is a maximal substring consisting of non-space 
+// characters only.
+
 function lengthOfLastWord(w) {
     let word = w.trim().split(' ').pop()
     console.log(word.length, 'lengthOfLastWord.js')
