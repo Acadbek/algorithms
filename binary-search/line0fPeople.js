8fc3aa942c2eb6d3c87b540127c885f846ae2c66
Asadbek <a.nosirjonov@uic.group>
2022-05-07 11:11:45 +0500

    feat: Give me a diamond

--- binary-search/line0fPeople.js
+++
@@ -1,5 +0,0 @@
-class Solution {
-    solve(n, a, b) {
-        return n - Math.max(a + 1, n - b) + 1;
-    }
-}
---
+++ codewars/GiveMeADiamond.js
@@ -0,0 +1,46 @@
+function diamond(n) {
+	if (n < 0) return null
+
+    if ( n % 2 !== 0 ) {
+				let down = false
+				let acc = 1
+        let res = Array(n).fill("")
+				for (let i = 0; i < n; i++) {
+					res[i] = res[i] + res[i].padStart(Math.ceil((n-acc)/2), " ") + '*'.repeat(acc) + '\n'
+					console.log(res)
+					if (down) {
+						console.log('subtracting', acc, n)
+						acc-=2 
+					} else {
+						console.log('adding', acc, n)
+						acc+=2
+						if (acc >= n) {
+							down = true
+						}
+					}
+				}
+				console.log('--------')
+				console.log(res)
+				consoleAll(res)
+				console.log('--------')
+    }else {
+      console.log(null)
+    }
+  }
+
+	function consoleAll(arr) {
+		arr.forEach(element => {
+			console.log(element)
+		});
+	}
+
+  diamond(9)
+  // diamond(5)
+  // diamond(-3)
+  // diamond(9)
+  // diamond(2)
+	//   *
+	//  ***
+	// *****
+	//  ***
+	//   *
