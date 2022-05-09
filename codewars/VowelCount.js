e0324b410b06b09aa67111a7338a469c64e458a1
Asadbek <a.nosirjonov@uic.group>
2022-05-09 12:41:50 +0500

    feat: vowel count

---
+++ codewars/VowelCount.js
@@ -0,0 +1,13 @@
+function getCount(str) {
+    let vowelsCount = 0
+    const vowels = ["a", "e", "i", "o", "u"]
+    for(let char of str) {
+        if(vowels.includes(char)) {
+            vowelsCount++
+        }
+    }
+  
+    return vowelsCount;
+}
+
+getCount("abracadabra")
