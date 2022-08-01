7caef67d42ad81ac9fa72c631705e73edd950567
Asadbek <a.nosirjonov@uic.group>
2022-08-02 01:42:39 +0500

    find nearest square number

--- codewars/nearestSq.js
+++ codewars/nearestSq.js
@@ -14,12 +14,13 @@
 // 16 (=4×4)
 // 25 (=5×5)
 
-const nearest_sq = n => console.log(Math.pow(Math.round(Math.sqrt(n)), 2));
+const nearest_sq = n => Math.pow(Math.round(Math.sqrt(n)), 2);
 nearest_sq(122)
 
 // Math.pow = 2ta argument qabul qiladi, birinchi arg ga son beriladi 2 arg esa 1 argni ozida 
 // nechi son bolsa shuncha bir biriga kopaytiradi Masalan: Math.pow(5, 5) bolsa 5 * 5 = 25 bolmaydi
-// 5 ni 5 martta oziga kopaytiradi javob 3125 chiqadi.
+// 5 ni 5 martta oziga kopaytiradi javob 3125 chiqadi. 
+// Buni 5**2 bu usul bilan ham yozsa boladi
 
 // Math.round = kasr sonni butun eng yaqin butun qismini qaytaradi --> 5.3 = 5
 // 4.7 = 5
