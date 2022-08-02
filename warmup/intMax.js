ebca9dba64b4ac69d55d88f74826f5ef8e072cf8
Asadbek <a.nosirjonov@uic.group>
2022-08-02 21:14:53 +0500

    return the largest number

---
+++ warmup/intMax.js
@@ -0,0 +1,11 @@
+// Given three int values, a b c, return the largest.
+
+// -------------------------------Uzbek--------------------------------
+
+// Butun sonlar beriladi ularni ichidan eng kattasini qaytarishingiz kerak
+
+function intMax(a, b, c) {
+	console.log(a > Math.max(b, c) ? a : Math.max(b, c));
+}
+
+intMax(102000000, 7, 50000000)
