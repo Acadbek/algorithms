3c64699639c8618668a329f97d8a2ddd3caf650c
Asadbek <a.nosirjonov@uic.group>
2022-08-02 22:31:35 +0500

    feat: title

---
+++ warmup/close10.js
@@ -0,0 +1,24 @@
+// Given 2 int values, return whichever value is nearest to the value 
+// 10, or return 0 in the event of a tie. Note that Math.abs(n) returns the absolute value 
+// of a number.
+
+// Examples
+
+// close10(8, 13) → 8
+// close10(13, 8) → 8
+// close10(13, 7) → 0
+
+// --------------------------------------------IN UZBEK----------------------------------
+
+// 2 int qiymati berilgan bo'lsa, qaysi qiymat 10 qiymatiga yaqin bo'lsa, uni qaytaring yoki 
+// teng bo'lsa 0 ni qaytaring. E'tibor bering, Math.abs(n) raqamning mutlaq qiymatini qaytaradi.
+
+// Misollar
+
+// close10(8, 13) → 8
+// close10(13, 8) → 8
+// close10(13, 7) → 0
+
+function close10(a, b) {
+  return
+}
