6fdfe03220025faa52cd50544d0e895122b5ac5e
Asadbek <a.nosirjonov@uic.group>
2022-08-13 20:10:56 +0500

    fix

--- codewars/sumTwoSmallestNumbers.js
+++ codewars/sumTwoSmallestNumbers.js
@@ -1,7 +1,38 @@
+//[English]
+
+// Create a function that returns the sum of the two lowest positive 
+// numbers given an array of minimum 4 positive integers. No floats or
+// non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+// ----------------------------------------------------------------------------------------------
+
+// [UZBEK]
+
+// Minimal 4 ta butun sonli massiv bor, ular ichida 2ta eng kichiklarini 
+// yig'indisi toping.
+
+
+// 1 - functionda sort(), slice(), reduce() ishlatilgan.
+// 2 - functionda sort() methodini o'zi ishlatilgan.
+
 function sumTwoSmallestNumbers(numbers) {
 	return numbers.sort((a, b) => a - b).slice(0, 2).reduce(function (a, b) {
 		return a + b
 	})
 }
 
-sumTwoSmallestNumbers([3, 87, 45, 12, 7])
+sumTwoSmallestNumbers([3, 87, 45, 12, 7, 1])
+
+
+
+
+const sumTwoSmallestNumbers2 = (nums) => {
+	let [a, b] = nums.sort((a, b) => a - b)
+	console.log(a + b);
+}
+
+sumTwoSmallestNumbers2([3, 87, 45, 12, 7, 1])
