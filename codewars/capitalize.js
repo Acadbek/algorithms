445595e959553ef85f66869fcae8931403c21b50
Asadbek <asadbeknan5566@gmail.com>
2023-02-27 17:37:15 +0500

    feat: fix string case

--- codewars/capitalize.js
+++ codewars/capitalize.js
@@ -1,13 +1,16 @@
-function capitalize(s, arr) {
-  let result = "";
-  for (let i = 0; i < s.length; i++) {
-    if (arr.includes(i)) {
-      result += s[i].toUpperCase();
-    } else {
-      result += s[i];
-    }
-  }
-  return result;
-}
+// function capitalize(s, arr) {
+//   let result = "";
+//   for (let i = 0; i < s.length; i++) {
+//     if (arr.includes(i)) {
+//       result += s[i].toUpperCase();
+//     } else {
+//       result += s[i];
+//     }
+//   }
+//   return result;
+// }
 
-capitalize('hello', [0, 3, 4])
+
+let capitalize = (s, a) => s.split``.map((c, i) => a.includes(i) ? c.toUpperCase() : c).join``;
+
+console.log(capitalize('hello', [1, 3, 4]));
