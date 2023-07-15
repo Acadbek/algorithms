a67136aabeb94f047221eb253f033cc21a64f7b7
asadbek <ibnnumon@gmail.com>
2023-07-15 16:52:28 +0500

    bitwise xor

---
+++ codesignal/extraNumber.js
@@ -0,0 +1,19 @@
+// You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. What is the value of the third integer?
+
+// Example
+
+// For a = 2, b = 7, and c = 2, the output should be
+// solution(a, b, c) = 7.
+
+// The two equal numbers are a and c.The third number(b) equals 7, which is the answer.
+
+function solution(a, b, c) {
+  if (a === b) return c;
+  else if (c === b) return a;
+  else if (c === a) return b;
+}
+
+const solution2 = (a, b, c) => a ^ b ^ c // optimize way
+
+
+solution(2, 2, 7)
--- codesignal/knapsackLight.js
+++ codesignal/knapsackLight.js
@@ -1,5 +1,6 @@
 function solution(value1, weight1, value2, weight2, maxW) {
   const canTakeBoth = weight1 + weight2 <= maxW;
+  
   const valueIfTakeBoth = canTakeBoth ? value1 + value2 : 0;
   const valueIfTakeFirst = weight1 <= maxW ? value1 : 0;
   const valueIfTakeSecond = weight2 <= maxW ? value2 : 0;
