751de5c61f0d42490fcbcbe98b8035039e9a2f26
asadbek <ibnnumon@gmail.com>
2023-07-17 17:35:37 +0500

    binary search

--- leetcode/removeduplicatesfromSortedArray.js
+++ leetcode/removeduplicatesfromSortedArray.js
@@ -1,27 +1,27 @@
-function removeDuplicates(nums) {
-  let k = 0; // Index of the last unique element
+// function removeDuplicates(nums) {
+//   let k = 0; // Index of the last unique element
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== nums[i + 1]) {
-      nums[k] = nums[i]; // Move the unique element to the next position
-      k++;
-    }
-  }
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== nums[i + 1]) {
+//       nums[k] = nums[i]; // Move the unique element to the next position
+//       k++;
+//     }
+//   }
 
-  for (let i = k; i < nums.length; i++) {
-    nums[i] = '_';
-  }
+//   for (let i = k; i < nums.length; i++) {
+//     nums[i] = '_';
+//   }
 
-  return k;
-}
+//   return k;
+// }
 
-const nums = [1, 1, 2]
-const k = removeDuplicates(nums);
+// const nums = [1, 1, 2]
+// const k = removeDuplicates(nums);
 
-console.log(k); // Output: 5
-console.log(nums); // Output: [0, 1, 2, 3, 4, '_', '_', '_', '_', '_']
+// console.log(k); // Output: 5
+// console.log(nums); // Output: [0, 1, 2, 3, 4, '_', '_', '_', '_', '_']
 
 
-let arr = [11, 22, 333, 44, 11111, 22222]
+// let arr = [11, 22, 333, 44, 11111, 22222]
 
-console.log(arr[3]);
+// console.log(arr[3]);
---
+++ leetcode/searchInsertPosition.js
@@ -0,0 +1,13 @@
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length;
+  while (left < right) {
+    const mid = (left + right) >> 1;
+    if (nums[mid] >= target) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+};
