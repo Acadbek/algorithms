998122b1bdac35b3422e987bb56cda67bd57f586
Asadbek <ibnnumon@gmail.com>
2024-01-05 13:23:29 +0500

    check performance time

--- codewars/pangram.js
+++ codewars/pangram.js
@@ -1,4 +1,6 @@
 function isPangram(string) {
+  const startTime = performance.now();
+
   const alphabet = new Set('abcdefghijklmnopqrstuvwxyz') // alfabitdagi hamma xarflarni set qilish
 
   string = string.toLowerCase().replace(/[^a-z]/g, '') // input ichidan faqat a-z gacha xarflarni qoldirish & kichik xarfga otkazish
@@ -7,21 +9,32 @@ function isPangram(string) {
     alphabet.delete(letter) // alfabitda bor bolgan inputni xarflarini ochirib yuborish
 
     if (alphabet.size === 0) { // tekshirish: agar inputni ichida alfabitdagi hamma xarf bor bolsa demak size 0 ga teng boladi
-      return true
+      console.log(true);
     }
   }
-  return false // inputda biror xarf yetishmasa alphabet ozgaruvchisida delete bolmay qolib ketadi va false beradi
+  const endTime = performance.now();
+  const executionTime = endTime - startTime;
+  console.log("Execution time:", executionTime, "milliseconds");
+
+  return false; // inputda biror xarf yetishmasa alphabet ozgaruvchisida delete bolmay qolib ketadi va false beradi
 }
 
-isPangram('The quick brown fox jumps over the lazy dog')
+// isPangram('The quick brown fox jumps over the lazy dog');
 
 // abcdefghijklmnopqrstuvwxyz
 
 
-// maybe best practice!
+// Maybe best practice!
 
-// function isPangram(string) {
-//   const alphabetList = [...'abcdefghijklmnopqrstuvwxyz'];
+function isPangram(string) {
+  const start = performance.now()
 
-//   return alphabetList.every((letter) => string.toLowerCase().includes(letter));
-// }
+  const alphabetList = [...'abcdefghijklmnopqrstuvwxyz'];
+
+  console.log(alphabetList.every((letter) => string.toLowerCase().includes(letter)));
+  const end = performance.now()
+  const time = end - start
+  console.log(time);
+}
+
+isPangram('The quick brown fox jumps over the lazy dog');
